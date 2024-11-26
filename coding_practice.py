7ed0ac36ad090ed2cf028e52945464dce943bffe
Centivien <mvcalcoriza2021@plm.edu.ph>
2024-11-26 14:21:48 +0800

    commit

---
+++ coding_practice.py
@@ -0,0 +1,71 @@
+# check if the string is polindrome
+# def string_poli(text):
+#     if text == text[::-1]:
+#         print("Text is a palindrome")
+#     else:
+#         print("Invalid")
+
+# # Get user input once
+# text = input("Enter a string: ")
+# string_poli(text)
+
+
+#print fizz if the number is multiple by 3 and print buzz if multiple by 5
+# def fizz_b():
+#     for i in range (1,101):
+#         if i % 3 == 0:
+#             print ('fizz')
+#         elif i % 5 == 0:
+#             print ('buzz')
+#         else:
+#             print(i)
+
+# fizz_b()
+    
+
+
+#count the vowels in inputed string
+# def vowels(text):
+#     vowel_count = 0
+#     # Define a set of vowels for easier checking
+#     vowels_set = {'a', 'e', 'i', 'o', 'u'}
+    
+#     for char in text.lower():  # Convert to lowercase for case-insensitive comparison
+#         if char in vowels_set:  # Check if the character is a vowel
+#             vowel_count += 1
+            
+#     return vowel_count  # Return the count
+
+# text = input("Enter a string: ")
+# count = vowels(text)
+# print("Number of vowels:", count)
+
+
+#reverse the inputed text
+# def rev(txt):
+#     inverse = txt[::-1]
+#     print(f'reverse: {inverse}')
+    
+# txt = input('input text:')
+# rev(txt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
